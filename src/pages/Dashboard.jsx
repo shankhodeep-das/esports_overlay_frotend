2bfe9dc6f5defad697fbfc502f6e25f66fcc5f1b
shankhodeep-das <shankhodeepdas609@gmail.com>
2026-05-01 19:16:27 +0530

    hh

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from "../context/AuthContext";
